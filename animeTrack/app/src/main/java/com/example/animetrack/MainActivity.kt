83454e12f59ef822b1f387c1fc296479f75297a5
Asier <asierserrano8@gmail.com>
2023-10-20 17:11:38 +0200

    Inicio HU 7 (Creación de una cuenta)
    Se ha comenzado HU 7.0.1 (GUI Login)

---
+++ animeTrack/app/src/main/java/com/example/animetrack/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.animetrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen)
+    }
+}
